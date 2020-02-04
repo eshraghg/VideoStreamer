94ff24dfa1bdb26bfe1f08b35bb5acdc8953aa2d
eshraghg <e.ghoodjani@gmail.com>
2020-02-04 15:30:42 +0330

    stream list halfway

---
+++ src/reducers/streamReducer.js
@@ -0,0 +1,26 @@
+import _ from "lodash";
+
+import {
+  CREATE_STREAM,
+  FETCH_STREAMS,
+  FETCH_STREAM,
+  DELETE_STREAM,
+  EDIT_STREAM
+} from "../actions/types";
+
+export default (state = {}, action) => {
+  switch (action.type) {
+    case FETCH_STREAMS:
+      return { ...state, ..._.mapKeys(action.payload, "id") }; // creates new object, {...obj1, ...obj2} which obj1 is old state and obj2 is response from server that is converted to object by mapkeys
+    case FETCH_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    case CREATE_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    case EDIT_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    case DELETE_STREAM:
+      return _.omit(state, action.payload); //in this case, action.payload is id (look actions)
+    default:
+      return state;
+  }
+};
